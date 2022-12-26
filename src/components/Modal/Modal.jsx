5c0c5f8eadb0819c3c9ca50bcb35598f789d4f41
Tetiana Deineka <tata4ka666@i.ua>
2022-12-26 21:27:58 +0200

    Update Modal.jsx

--- src/components/Modal/Modal.jsx
+++ src/components/Modal/Modal.jsx
@@ -1,40 +1,31 @@
-import React, { Component } from 'react';
+import React, { useEffect } from 'react';
 import css from './Modal.module.css';
-//import propTypes from 'prop-types';
 
-export class Modal extends Component {
-  componentDidMount() {
-    window.addEventListener('keydown', this.handleKeyDown);
-  }
-
-  componentWillUnmount() {
-    window.removeEventListener('keydown', this.handleKeyDown);
-  }
-
-  handleKeyDown = e => {
-    if (e.code === 'Escape') {
-      this.props.closeModal();
-    }
-  };
-
-  handleClickOverlay = e => {
+export const Modal = ({ closeModal, modalImg, modalAlt }) => {
+  
+  useEffect(() => {
+    const handleKeyDown = event => {
+      if (event.code === 'Escape') {
+        closeModal();
+      }
+    };
+    window.addEventListener('keydown', handleKeyDown);
+    return () => {
+      window.removeEventListener('keydown', handleKeyDown);
+    };
+  }, [closeModal]);
+  
+  const handleClickOverlay = e => {
     if (e.currentTarget === e.target) {
-      this.props.closeModal();
+      closeModal();
     }
   };
 
-  render() {
     return (
-      <div className={css.Overlay} onClick={this.handleClickOverlay}>
+      <div className={css.Overlay} onClick={handleClickOverlay}>
         <div className={css.Modal}>
-          <img src={this.props.modalImg} alt={this.props.modalAlt} />
+          <img src={modalImg} alt={modalAlt} />
         </div>
       </div>
     );
-  }
 } 
-// Modal.propTypes = {
-//   src: propTypes.string.isRequired,
-//   alt: propTypes.string.isRequired,
-//   closeModal: propTypes.func.isRequired,
-// };
