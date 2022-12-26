583d1fe381d7f203cd7981d02ed920774cea724a
Tetiana Deineka <tata4ka666@i.ua>
2022-12-26 20:42:50 +0200

    Update App.jsx

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,16 +1,101 @@
-export const App = () => {
-  return (
-    <div
-      style={{
-        height: '100vh',
-        display: 'flex',
-        justifyContent: 'center',
-        alignItems: 'center',
-        fontSize: 40,
-        color: '#010101'
-      }}
-    >
-      React homework template
-    </div>
-  );
-};
+import React, { Component } from "react";
+import { Searchbar } from "./Searchbar/Searchbar"
+import { fetchImages } from "./pixabay-api";
+import { ImageGallery } from "./ImageGallery/ImageGallery";
+import { Button } from "./Button/Button";
+import { Loader } from './Loader/Loader';
+import { Modal } from './Modal/Modal';
+
+export class App extends Component {
+  state = {
+    query: '',
+    page: 1,
+    images: [],
+    totalHits: null,
+    isLoading: false,
+    showModal: false,
+    modalImg: '',
+    modalAlt: '',
+  };
+
+  async componentDidUpdate(prevProps, prevState) {
+    try {
+      if (
+        this.state.query !== prevState.query ||
+        this.state.page !== prevState.page
+      ) {
+        this.setState({ isLoading: true });
+
+        const data = await fetchImages(this.state.query, this.state.page);
+        const { hits, totalHits } = data;
+
+        this.setState({
+          images: [...this.state.images, ...hits],
+          totalHits,
+          isLoading: false,
+        });
+      }
+    } catch (error) {
+      this.setState({ isLoading: false });
+    }
+  }
+
+  handleFormSubmit = ({ query, page, images }) => {
+    this.setState({
+      images,
+      query,
+      page,
+    });
+  };
+
+  loadMore = () => {
+    this.setState(prevState => ({
+      page: prevState.page + 1,
+    }));
+  };
+
+  toggleModal = (modalImg, modalAlt) => {
+    this.setState(prevState => ({
+      showModal: !prevState.showModal,
+      modalImg,
+      modalAlt,
+    }));
+  };
+
+  render() {
+    return (
+      <div
+        style={{
+          display: 'grid',
+          gridTemplateColumns: '1fr',
+          gridGap: '16px',
+          paddingBottom: '24px',
+        }}
+      >
+        {this.state.isLoading ? (
+          <Loader />
+        ) : (
+          <React.Fragment>
+            <Searchbar onSubmit={this.handleFormSubmit} />
+            <ImageGallery
+              images={this.state.images}
+              openModal={this.toggleModal}
+            />
+            {this.state.totalHits / 12 >= this.state.page &&
+              !this.state.isLoading && (
+                <Button onClick={this.loadMore} />
+              )}
+            {this.state.isLoading && <Loader />}
+          </React.Fragment>
+        )}
+        {this.state.showModal && (
+          <Modal
+            modalImg={this.state.modalImg}
+            modalAlt={this.state.modalAlt}
+            closeModal={this.toggleModal}
+          />
+        )}
+      </div>
+    );
+  }
+}
