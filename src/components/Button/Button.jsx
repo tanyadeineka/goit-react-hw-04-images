9feb125f86b9a2a8ca51997d994ec8b88c9c8280
Tetiana Deineka <tata4ka666@i.ua>
2022-12-26 20:42:47 +0200

    Create Button.jsx

---
+++ src/components/Button/Button.jsx
@@ -0,0 +1,14 @@
+import propTypes from 'prop-types';
+import css from './Button.module.css';
+
+export const Button = ({ onClick }) => {
+  return (
+    <button className={css.Button} onClick={onClick} type="button">
+      Load more
+    </button>
+  );
+}
+
+Button.propTypes = {
+  onClick: propTypes.func.isRequired,
+};
